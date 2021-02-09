bdb8d59bc8b01225472e8397d6aff9b6918c58ea
jisoooo17 <jisoooo17@gmali.com>
2021-02-09 15:31:48 +0900

    header_sub적용

--- src/sub/header/header.js
+++ src/sub/header/header.js
@@ -1,47 +1,11 @@
 $(document).ready(function(){
-    var $header = $("#header_sub");
-    var $gnb_li = $("#gnb>li");
-    var $sub = $gnb_li.find(".sub");
-    var speed = 500;  
-
-    $gnb_li.on("mouseenter focusin",function(){
-        openSub(this);
+    $("#gnb>li").on("mouseenter focusin",function(){
+        $(this).children("ul").stop().slideDown();
+        $(this).children("a").addClass("on");
     });
 
-    $gnb_li.on("mouseleave focusout",function(){
-        closeSub(this);
+    $("#gnb>li").on("mouseleave focusout",function(){
+        $(this).children("ul").stop().slideUp();
+        $(this).children("a").removeClass("on");
     });
-
-    function openSub(el){
-        var ht = $sub.outerHeight();
-        var bg = $sub.css("background-color");
-        var posY = $header.outerHeight();
-        var isBgGnb = $(".bgGnb").length;//비지지앤비없으면 false인 0이 찍힘
-        console.log(isBgGnb);
-
-        if(!isBgGnb) { 
-            console.log("test");       
-            $header.prepend(
-                $("<div class='bgGnb'>")
-                    .css({
-                        width:"100%",  height:ht, backgroundColor:bg, 
-                        position:"absolute", left:0, top:posY, display:"none"
-                    })
-            )
-        }
-
-        
-        $(el).children(".sub").stop().slideDown(speed);
-        $(".bgGnb").stop().slideDown(speed);
-        $(el).children("a").addClass("on");
-    }
-
-    function closeSub(el){
-        $(el).children(".sub").stop().slideUp(speed/2);
-        $(".bgGnb").stop().slideUp(speed/2,function(){
-            $(this).remove();
-        });
-        $(el).children("a").removeClass("on");
-    }
-    
 });
